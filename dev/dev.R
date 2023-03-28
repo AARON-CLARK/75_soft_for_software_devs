d575c0423528abe0763b2a57c8120ceb5a08ff5d
AARON-CLARK <clark.aaronchris@gmail.com>
2023-03-28 08:48:42 -0400

    initialize a decent README with talk outline, initialize an xaringan

---
+++ dev/dev.R
@@ -0,0 +1,9 @@
+
+usethis::use_readme_rmd()
+
+# print to pdf
+
+# remotes::install_github('rstudio/pagedown')
+# install.packages("pagedown") # works fine
+# pagedown::chrome_print('tracking_usage_pres.html')
+pagedown::chrome_print('tracking_usage_pres_crug.html')
